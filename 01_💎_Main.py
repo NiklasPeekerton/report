22f3eeec7bd893c2094711adf52abf541d8f6c36
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-07-08 17:51:27 +0200

    Update 01_💎_Main.py

--- 01_💎_Main.py
+++ 01_💎_Main.py
@@ -34,7 +34,7 @@ full = giveme()
 st.write(full)
 
 fig = px.bar(uhaulslim, x="Month"
-             , y=["All Devices",'Desktop','Mobile'] title="test"
+             , y=["All Devices",'Desktop','Mobile'], title="test"
             #labels=dict(value="Average overall points", variable="Metrics"), height=600
             )
 st.plotly_chart(fig, use_container_width=True)
