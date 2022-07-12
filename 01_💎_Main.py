3ba454e4dc0d02224914a979e8879ee670ceb6d9
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-07-12 15:16:29 +0200

    Update 01_💎_Main.py

--- 01_💎_Main.py
+++ 01_💎_Main.py
@@ -51,9 +51,11 @@ dict = {
 
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("Uhaul traffic", numerize.numerize(metrics['Size'][0]))
-col2.metric("Uhaul p1 value", metrics['p1 R-Value'][0])
+col2.metric("Uhaul p1 value", "{:.0%}".format(metrics['p1 R-Value'][0]))
 col3.metric("Uhaul p2 value", metrics['p2 R-Value'][0])
 col4.metric("Uhaul p3 value", metrics['p3 R-Value'][0])
+
+#percentage = "{:.0%}".format(metrics['p1 R-Value'][0])
 
 st.metric("Uhaul traffic", numerize.numerize(metrics['Size'][0]))
 test = metrics['Size'][0]
