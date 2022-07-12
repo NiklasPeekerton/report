afe5699026de88d8f21b1533b3d39909d0f8ee7b
NiklasPeekerton <64794618+NiklasPeekerton@users.noreply.github.com>
2022-07-12 11:01:36 +0200

    Update 01_💎_Main.py

--- 01_💎_Main.py
+++ 01_💎_Main.py
@@ -50,7 +50,7 @@ dict = {
 }
 #st.metric("Uhaul traffic", numerize(metrics['Size'][0]))
 test = metrics['Size'][0]
-st.write(numerize(1290491241))
+st.write(numerize.numerize(1290491241))
 
 
 def graph(company):
